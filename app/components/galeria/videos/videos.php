3e1a389f9715a0dc2875682d198acd8f64e224b8
Mateus Bougleux <mateusbougleuxmaia@hotmail.com>
2023-03-14 18:18:12 -0300

    chore: videos

---
+++ app/components/galeria/videos/videos.php
@@ -0,0 +1,9 @@
+<div>
+  <div class="col-md-12">
+    <iframe width="708" height="472"  src="https://www.youtube.com/embed/J42SZXS-_Qo" title="ASMR Programming - Coding a Snake Game - No Talking" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+
+  <div class="col-md-12">
+    <iframe width="708" height="472"  src="https://www.youtube.com/embed/GXlckaGr0Eo" title="ASMR Programming - Coding Pacman - No Talking" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  </div>
+</div>
---
+++ app/pages/videos.php
@@ -0,0 +1,7 @@
+<?require_once('../components/top.php');?>
+
+    <div class="container d-flex justify-content-center">
+        <? require_once('../components/galeria/videos/videos.php'); ?>
+    </div>
+
+<?require_once('../components/footer.php');?>
